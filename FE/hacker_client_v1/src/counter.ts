9e5173db6612e48e03e75ed46074134d14ba03e5
ronh <2039858744@qq.com>
2024-04-21 11:25:49 +0800

    :sparkles: test: use rust invoke

--- FE/hacker_client_v1/src/counter.ts
+++ FE/hacker_client_v1/src/counter.ts
@@ -1,8 +1,14 @@
+import { invoke } from '@tauri-apps/api';
+
 export function setupCounter(element: HTMLButtonElement) {
-  let counter = 0
+  let counter = 0, str = '';
   const setCounter = (count: number) => {
+    invoke('hello', { name: 'world'}).then((res) => {
+      console.log(res);
+      str = res as string;
+    })
     counter = count
-    element.innerHTML = `count is ${counter}`
+    element.innerHTML = `count is ${counter}, str is ${str}`
   }
   element.addEventListener('click', () => setCounter(counter + 1))
   setCounter(0)
