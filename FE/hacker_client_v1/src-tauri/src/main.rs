00661ad7256ae719cbebc7b157a39782ac9438be
ronh <2039858744@qq.com>
2024-04-26 10:03:20 +0800

    :sparkles: feat: use mod

--- FE/hacker_client_v1/src-tauri/src/main.rs
+++ FE/hacker_client_v1/src-tauri/src/main.rs
@@ -1,14 +1,12 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-#[tauri::command]
-fn hello(name: &str) -> String {
-  format!("Hello, {} from Rust!", name)
-}
+// 导入命令模块
+mod command;
 
 fn main() {
   tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![hello])
+    .invoke_handler(tauri::generate_handler![command::hello])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
